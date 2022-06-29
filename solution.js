036651a9667f5debb5a897e3b08441e369be59a1
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-06-29 16:12:19 +0200

    ex 3

---
+++ solution.js
@@ -0,0 +1,26 @@
+let apples = 20;
+let oranges = 30;
+console.log("Check whether apples and oranges are equal.", apples === oranges);
+
+console.log("Check whether apples and oranges are not equal.", apples !== oranges);
+
+console.log("Check whether apples is greater than oranges.", apples > oranges);
+
+console.log("Check whether apples is less than or equal to oranges.", apples <= oranges);
+
+console.log("Check whether oranges is greater than apples.", oranges > apples);
+
+let mangoes = 5;
+
+console.log("Multiply mangoes by apples, and check whether this result is greater than mangoes added to oranges.", mangoes * apples  > mangoes + oranges);
+
+console.log("Subtract mangoes from apples and check whether this result is less than oranges divided by mangoes.", apples - mangoes < oranges / mangoes);
+
+console.log("Check whether mangoes, apples and oranges are equal.", mangoes === apples && apples === oranges);
+
+console.log("Check whether the remainder of apples divided by mangoes and the remainder of oranges divided by mangoes are equal.", apples % mangoes === oranges % mangoes);
+
+console.log("Check whether mangoes added to apples is greater than oranges minus mangoes. ", apples + mangoes > oranges - mangoes);
+
+console.log("If it is not, find an operator which will give a result of true.", apples + mangoes === oranges - mangoes);
+
